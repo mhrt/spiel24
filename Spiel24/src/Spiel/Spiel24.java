94a57ff577fe867b25b1c80b7afbbc8ff21bb92d
mhrt <mi.hartmann@ostfalia.de>
2016-11-08 09:56:32 +0100

    Neue Klasse

--- Spiel24/src/Spiel/Spiel24.java
+++ Spiel24/src/Spiel/Spiel24.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
   *
   * 24 (Spiel)
   *
-  * @version 1.01 vom 08.11.2016
+  * @version 1.0 vom 08.11.2016
   * @author Michael Hartmann
   */
   
---
+++ Spiel24/src/Spiel/Test.java
@@ -0,0 +1,13 @@
+package Spiel;
+
+/**
+*
+* Testklasse
+*
+* @version 1.0 vom 08.11.2016
+* @author Michael Hartmann
+*/
+
+public class Test {
+
+}
